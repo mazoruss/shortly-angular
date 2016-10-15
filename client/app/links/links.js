b23419ed7907318ef6ed49147ad8bb9e71645cf1
Jace <chesszhu@gmail.com>
2016-10-14 18:51:48 -0700

    complete links display

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,11 +1,19 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
+	$scope.search = '';
   $scope.data = {};
+  $scope.display = [];
+
   Links.getAll().then(function(links) {
     $scope.data.links = links;
+    $scope.display = links.sort( (x, y) => y.visits - x.visits);
     console.log(JSON.stringify(links));
   });
+
+	$scope.filter = function() {
+		$scope.display = $scope.data.links.filter( x => x.title.toLowerCase().includes($scope.search));
+	}
 
   $scope.getUrl = function(link) {
   	return link.baseUrl + '/' + link.code;
